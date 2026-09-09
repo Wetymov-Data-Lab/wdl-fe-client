8305e8cbedba0ee887d33b005558501cde31f014
wetymov <wetymov.dev@gmail.com>
2026-09-09 15:26:26 +0300

    fix(editor): add hover effect to edge

--- src/widgets/diagram/ui/diagram-editor.tsx
+++ src/widgets/diagram/ui/diagram-editor.tsx
@@ -185,7 +185,7 @@ export function DiagramEditor({
         defaultEdgeOptions={{ type: "smoothstep" }}
         connectionLineStyle={{ stroke: "var(--color-accent)", strokeWidth: 2 }}
         proOptions={{ hideAttribution: true }}>
-        <Background variant={BackgroundVariant.Lines} gap={20} size={10} color="var(--color-diagram-grid)" />
+        <Background variant={BackgroundVariant.Lines} gap={20} size={2} color="var(--color-diagram-grid)" />
         <Controls showInteractive={false} position="bottom-right" />
       </ReactFlow>
     </section>
